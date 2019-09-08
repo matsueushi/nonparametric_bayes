f1ded486aba342adbf919db2e22ad43f696176e0
matsueushi <matsueushi@gmail.com>
2019-09-08 13:31:05 -0400

    first commit

---
+++ kmean.jl
@@ -0,0 +1,29 @@
+using Distributions
+
+mutable struct KMean
+    points::Array{Float64}
+    K::Int64
+    zs::Vector{Int64}
+    μs::Array{Float64}
+    dist::Float64
+    dist_change::Float64
+    KMean(points::Array{Float64}, K::Int64) = new(points, K, zeros(size(points, 1)), zeros(K, size(points, 2)), Inf, Inf)
+end 
+
+function update!(km::KMean)
+    diff_sq = (km.points .- reshape(km.μs', 1, size(km.μs, 2), :)).^2
+    norms = dropdims(sum(diff_sq, dims = 2), dims = 2)
+    amin = argmin(norms, dims = 2)
+    km.zs = vec([x[2] for x in amin])
+    if Base.length(unique(km.zs)) != km.K
+        min, max = minimum(km.points, dims = 1), maximum(km.points, dims = 1)
+        init_μs = mapslices(x->rand(Uniform(x...), km.K), vcat(min, max), dims = 1)
+        km.μs = init_μs
+    else
+        km.μs = vcat([mean(km.points[km.zs .== i, :], dims = 1) for i in 1:km.K]...)
+    end
+    new_dist = sum(norms[amin])
+    km.dist_change = km.dist - new_dist
+    km.dist = new_dist
+end
+
